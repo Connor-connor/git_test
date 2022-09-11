9d0e98c32eeefc3fa9b9e276c05d7dd7ea72f1e2
lihaopeng <752869332@qq.com>
2022-09-11 21:40:00 +0800

    first idea commit

---
+++ src/com/connor/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.connor.git;
+
+/**
+ * @author Connor
+ * @date 2022/9/11 21:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
