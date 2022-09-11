204ee4b930f6441656136bacf2a994ee000e9913
lihaopeng <752869332@qq.com>
2022-09-11 21:42:31 +0800

    second idea commit

--- src/com/connor/git/GitTest.java
+++ src/com/connor/git/GitTest.java
@@ -7,5 +7,6 @@ package com.connor.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
